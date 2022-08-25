5a6a3953b03312c994bc10ebae70ee2f230dc4f1
Noemi Gutierrez <noemigp0@gmail.com>
2022-08-24 22:21:15 -0500

    Adding null safe operator

--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -3,7 +3,7 @@ import { useEffect, useState } from "react"
 export default function PokemonCard(props) {
     const { img, name} = props  //use dry --Dont repeat yourself
     
-    const [pokemon, setPokemon] = useState({ sprites: {front_default: ""}})
+    const [pokemon, setPokemon] = useState({})
     useEffect(() => {
         fetch(props.url)
         .then((response) => response.json())
@@ -12,7 +12,7 @@ export default function PokemonCard(props) {
     return (
         <article className="pokemon-card">
         <header>
-          <img src={pokemon.sprites.front_default} alt="pokemon image"></img>
+          <img src={pokemon?.sprites?.front_default} alt="pokemon image"></img>
         </header>
         <footer>
           <p>{props.name}</p>
